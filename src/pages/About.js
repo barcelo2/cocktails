11aaff5e3fb41a62c15339052984317650a813c0
Vera Pushkash <berunchikp@gmail.com>
2021-10-20 12:17:53 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam labore. Laborum autem voluptatem iste, ut reiciendis totam sint, ullam esse fugiat aliquid velit natus eligendi est asperiores debitis. Earum.</p>
+    </section>
+  )
+}
+
+export default About
